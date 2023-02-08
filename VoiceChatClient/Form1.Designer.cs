4d35473aa474b5e1fd2f2227df2ee70184371cc7
unknown <mncmilan@gmail.com>
2023-02-08 09:58:47 +0100

    Initial commit

---
+++ VoiceChatClient/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace VoiceChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.recordButton = new System.Windows.Forms.Button();
+            this.stopRecordButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.receiveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // recordButton
+            // 
+            this.recordButton.Location = new System.Drawing.Point(48, 54);
+            this.recordButton.Name = "recordButton";
+            this.recordButton.Size = new System.Drawing.Size(129, 63);
+            this.recordButton.TabIndex = 0;
+            this.recordButton.Text = "Record";
+            this.recordButton.UseVisualStyleBackColor = true;
+            this.recordButton.Click += new System.EventHandler(this.recordButton_Click);
+            // 
+            // stopRecordButton
+            // 
+            this.stopRecordButton.Location = new System.Drawing.Point(194, 54);
+            this.stopRecordButton.Name = "stopRecordButton";
+            this.stopRecordButton.Size = new System.Drawing.Size(129, 63);
+            this.stopRecordButton.TabIndex = 1;
+            this.stopRecordButton.Text = "Stop recording";
+            this.stopRecordButton.UseVisualStyleBackColor = true;
+            this.stopRecordButton.Click += new System.EventHandler(this.stopRecordButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(48, 136);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(275, 63);
+            this.playButton.TabIndex = 2;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(368, 54);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(164, 63);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // receiveButton
+            // 
+            this.receiveButton.Location = new System.Drawing.Point(368, 136);
+            this.receiveButton.Name = "receiveButton";
+            this.receiveButton.Size = new System.Drawing.Size(164, 63);
+            this.receiveButton.TabIndex = 4;
+            this.receiveButton.Text = "Receive";
+            this.receiveButton.UseVisualStyleBackColor = true;
+            this.receiveButton.Click += new System.EventHandler(this.receiveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 237);
+            this.Controls.Add(this.receiveButton);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.stopRecordButton);
+            this.Controls.Add(this.recordButton);
+            this.Name = "Form1";
+            this.Text = "Voice Chat";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button recordButton;
+        private System.Windows.Forms.Button stopRecordButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Button receiveButton;
+    }
+}
+
---
+++ VoiceChatClient/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+
+
+namespace VoiceChatClient
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string command, StringBuilder retstrign, int Returnlength, IntPtr callback);
+        
+        string recordingPath = "C:\\Users\\mncmilan\\Desktop\\test.wav";
+        string tempPath = "C:\\Users\\mncmilan\\Desktop\\temp.wav";
+
+        private const int bufSize = 100000000;
+        private State state = new State();
+        private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+        private AsyncCallback recv = null;
+        
+        Socket socket;
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            mciSendString("open new Type waveaudio alias recsound", null, 0, IntPtr.Zero);
+            recordButton.Click += new EventHandler(this.recordButton_Click);
+
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+            //Creamos el socket 
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            try
+            {
+                socket.Connect(ipep);//Intentamos conectar el socket
+            }
+            catch (SocketException)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+            
+        }
+
+        private void recordButton_Click(object sender, EventArgs e)
+        {
+            mciSendString("record recsound", null, 0, IntPtr.Zero);
+            this.BackColor = Color.Green;
+            stopRecordButton.Click += new EventHandler(this.stopRecordButton_Click); 
+
+        }
+
+        private void stopRecordButton_Click(object sender, EventArgs e)
+        {
+            mciSendString("save recsound " + recordingPath, null, 0, IntPtr.Zero);
+            mciSendString("close recsound", null, 0, IntPtr.Zero);
+            this.BackColor = Color.Empty;
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            (new Microsoft.VisualBasic.Devices.Audio()).Play(recordingPath);
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            byte[] voiceMessage = File.ReadAllBytes(recordingPath);
+            socket.Send(voiceMessage);
+            socket.BeginSend(voiceMessage, 0, voiceMessage.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = socket.EndSend(ar);
+                MessageBox.Show("sent");
+            }, state);
+                
+        }
+
+        private void receiveButton_Click(object sender, EventArgs e)
+        {
+            socket.BeginReceiveFrom(state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = socket.EndReceiveFrom(ar, ref epFrom);
+                socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+            }, state);
+            System.IO.File.WriteAllBytes(tempPath, state.buffer);
+            (new Microsoft.VisualBasic.Devices.Audio()).Play(tempPath);  
+                
+        }
+        
+    }
+}
