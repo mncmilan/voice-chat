9a016d5af9f39f0f6191e2b0a46d7ba39f517c79
unknown <mncmilan@gmail.com>
2023-02-14 21:10:38 +0100

    rename socket variable

--- voiceChatServer.c
+++ voiceChatServer.c
@@ -10,11 +10,11 @@
 int main(void) {
 	struct sockaddr_in serv_adr, si_other;
 	int BUFLEN = 8000;
-	int sock_listen, i, blen, slen = sizeof(si_other);
+	int sock, i, blen, slen = sizeof(si_other);
 	char buf[BUFLEN];
 	
-	sock_listen = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sock_listen == -1)
+	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock == -1)
 		printf("Error in socket connection\n");
 	
 	memset((char *) &serv_adr, 0, sizeof(serv_adr));
@@ -22,11 +22,11 @@ int main(void) {
 	serv_adr.sin_port = htons(9050);
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
-	if (bind(sock_listen, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
+	if (bind(sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
 		printf("Error binding\n");
 
 	for(;;){
-		blen = recvfrom(sock_listen, buf, sizeof(buf), 0, (struct sockaddr*) &si_other, &slen);
+		blen = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*) &si_other, &slen);
 		
 		if (blen == -1)
 			printf("Error receiving the message\n");
@@ -34,7 +34,7 @@ int main(void) {
 			printf("Message received\n");
 			
 			// Send answer back to the client
-			if (sendto(sock_listen, buf, blen, 0, (struct sockaddr*) &si_other, slen) == -1)
+			if (sendto(sock, buf, blen, 0, (struct sockaddr*) &si_other, slen) == -1)
 				printf("Error sending the message\n");
 			printf("Message sent\n\n");
 		}
